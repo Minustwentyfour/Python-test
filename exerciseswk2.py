59fc58ca25c76c1d3e935d55d72be9b0c6a50ca2
Ollieblocks <connelre@tcd.ie>
2020-10-07 22:23:53 +0000

    wk2

---
+++ exerciseswk2.py
@@ -0,0 +1,15 @@
+shopping_list = ["egg", "bread", "juice", "chocolate", "apples"] # Fill in with some values
+
+print(shopping_list) # Print the whole list
+
+print(1) # Figure out how to print individual values
+
+nutrition_facts = {"Fat" : "8g", "Cholesterol" : "0g", "Sodium" : "5mg", "Protein" : "2g"} # Fill in with the nutrition facts from the label
+
+print(nutrition_facts) # Print all the nutrition facts
+
+print(nutrition_facts["Fat"]) # Uncomment this line and pick a value to print individually
+
+age = input('What is your age?: ')
+
+print(2 * int(age))
---
+++ wk2.py
@@ -0,0 +1,23 @@
+name = "Leon"
+print(type(name))
+
+name="Mike"
+print(name)
+
+list = [1,2,3,4] # makes a list called list 
+print(type(list))
+
+print(list[1]) # calls the 2nd(index starts at 1) variable in the list
+
+list.append(5) # adds a 5 to the end of the list
+
+print(list)
+
+dict = {
+    "name":"John Doe", 
+    "age": 21, 
+    "net worth": 5213.4
+    }
+
+print(dict)
+print(dict["age"])
